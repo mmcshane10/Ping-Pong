659755d3f00c8f0f6d3b7ad0072e1a1b5e33cf34
Mike <mmcshane10@gmail.com>
2019-06-22 15:51:09 -0700

    latest update

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,28 @@ $(document).ready(function() {
     for (i=1; i<=number; i++)
     array1.push(i);
 
-    array1.forEach(function(array) {
-      if ((array % 5 === 0) && (array % 3 === 0)) {
-        return "pingpong"
-      } else if (array % 3 === 0) {
-        return "ping"
-      } else if (array % 5 === 0) {
-        return "pong"
-      } else {
-        $("array").append(array)
-      }
-    });
+    // array1.forEach(function(array) {
+    //   if ((array % 5 === 0) && (array % 3 === 0)) {
+    //     return "pingpong"
+    //   } else if (array % 3 === 0) {
+    //     return "ping"
+    //   } else if (array % 5 === 0) {
+    //     return "pong"
+    //   } else {
+    //     $("array").append(array)
+    //   }
+    // });
 
     for (var i =0; i < array1.length; i += 1) {
+      // if ((array1[i] % 5 === 0) && (array1[i] % 3 === 0)) {
+      //   array1[i] = "pingpong";
+      // } else if (array1[i] % 3 === 0) {
+      //   array1[i] = "ping"
+      // } else if (array1[i] % 3 === 0) {
+      //   array1[i] = "ping"
+      // } else {
+      //   return array1[i]
+      // }
       $("#result").append("<li>" + array1[i] + "</li>")
     }
 
